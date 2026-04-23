4fbff28648817e5a9fcb4cf0f02fb5bbcd9ce0b3
xsohio <rahmatefendi1703@gmail.com>
2026-04-23 08:33:14 +0700

    Update start.sh to configure nginx with dynamic port
    Add nginx configuration for dynamic port usage

--- start.sh
+++ start.sh
@@ -1,3 +1,28 @@
 #!/bin/sh
-php-fpm &
+
+# Gunakan PORT dari Railway, default 8080
+PORT=${PORT:-8080}
+
+# Tulis nginx config dengan port yang benar
+cat > /etc/nginx/http.d/default.conf << EOF
+server {
+    listen ${PORT};
+    root /var/www/html;
+    index index.php index.html;
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location ~ \.php$ {
+        fastcgi_pass 127.0.0.1:9000;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        include fastcgi_params;
+    }
+}
+EOF
+
+# Jalankan php-fpm dan nginx
+php-fpm -D
 nginx -g "daemon off;"
